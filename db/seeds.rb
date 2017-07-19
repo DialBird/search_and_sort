946aa1725a5bad931d1ee69b5fc2ebe6a649682c
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-07-19 10:55:35 +0000

    seedを作成する

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do
-  User.create(name: Faker::Name.name, age: 12, sex_id: 1)
+Corporation.create(name: 'Google')
+Corporation.create(name: 'Facebook')
+Corporation.create(name: 'Apple')
+
+# 会社に雇われているUser
+25.times do
+  User.create(name: Faker::Name.name,
+              age: Faker::Number.between(0, 80),
+              sex_id: Faker::Number.between(1, 3),
+              corporation_id: Faker::Number.between(1, 3))
+end
+
+# 会社に雇われていないUser
+25.times do
+  User.create(name: Faker::Name.name,
+              age: Faker::Number.between(0, 80),
+              sex_id: Faker::Number.between(1, 3))
+end
+
+# 今いるユーザーに対して適当にDogモデルを付与する
+user_cnt = User.count
+100.times do
+  Dog.create(user_id: Faker::Number.between(1, user_cnt))
 end
