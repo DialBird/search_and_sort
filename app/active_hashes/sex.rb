b4351796d66ffbbb5d71102e4a098abd95227a05
Keisuke Taniguchi <rememberthatk@icloud.com>
2017-07-24 05:01:49 +0000

    sexモデルのnameを修正する

--- app/active_hashes/sex.rb
+++ app/active_hashes/sex.rb
@@ -1,8 +1,8 @@
 class Sex < ActiveHash::Base
   include ActiveHash::Enum
   self.data = [
-    { id: 1, type: 'man', name: '男' },
-    { id: 2, type: 'woman', name: '女' },
+    { id: 1, type: 'man', name: '男性' },
+    { id: 2, type: 'woman', name: '女性' },
     { id: 3, type: 'other', name: 'それ以外' }
   ]
   enum_accessor :type
